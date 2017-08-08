f551033ffe0efd2614a207d4a234e3ced69ee22d
ssimons1 <sylviesimons@gmail.com>
2017-08-08 20:37:07 +0300

    #2
    Some implemented functions in python and edited sql

--- store.py
+++ store.py
@@ -1,12 +1,75 @@
 from bottle import route, run, template, static_file, get, post, delete, request
-from sys import argv
 import json
 import pymysql
+
+
+
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='root',
+                             db='store',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+cursor = connection.cursor();
 
 @get("/admin")
 def admin_portal():
 	return template("pages/admin.html")
 
+# List Categories
+@get("/categories")
+def list_categories():
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM categories;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return json.dumps({'STATUS': 'SUCCESS', 'CATEGORIES': result, 'CODE': 200})
+    except Exception:
+        return json.dumps({'STATUS': 'ERROR', 'MSG': 'Internal error', 'CODE': 500})
+
+# List All Products
+@get("/products")
+def list_all_products():
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM products;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return json.dumps({'STATUS':'SUCCESS','PRODUCTS':result, 'CODE': 200})
+    except Exception:
+        return json.dumps({'STATUS':'ERROR','MSG':'Internal error','CODE': 500})
+
+# List Products by Category
+@get("/category/<id>/products")
+def list_products_by_category(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM products WHERE category={};".format(id)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return
+    except Exception:
+        return
+
+
+
+
+
+# Creating a Category
+@post("/category")
+def create_category():
+    try:
+        with connection.cursor() as cursor:
+            new_category = request.POST.get("name")
+            sql_query = "INSERT INTO categories (id, name) VALUES (0, {})".format(new_category)
+            cursor.execute(sql_query)
+            result = cursor.lastrowid()
+            return json.dumps({'STATUS': 'SUCCESS', 'CAT_ID': result, 'CODE': 201})
+    except Exception:
+        pass
+        # return json.dumps({'STATUS':'ERROR','MSG':''})
 
 
 @get("/")
@@ -29,4 +92,8 @@ def images(filename):
     return static_file(filename, root='images')
 
 
-run(host='0.0.0.0', port=argv[1])
+def main():
+    run(host='localhost', port=7000)
+
+if __name__ == '__main__':
+    main()
--- store.sql
+++ store.sql
@@ -6,7 +6,7 @@ DROP database if exists store;
 
 -- create categories table with primary id key
 CREATE TABLE categories(
-id INT,
+id INT auto_increment,
 name VARCHAR(30),
 PRIMARY KEY (id)
 );
@@ -19,14 +19,14 @@ price DOUBLE,
 title VARCHAR(100),
 favorite BOOLEAN,
 img_url VARCHAR(100),
-id INT,
+id INT auto_increment,
 PRIMARY KEY (id)
 );
 
 -- insert categories into category table
-INSERT INTO categories VALUES (1, 'clothes'), (2, 'cars'), (3, 'food');
+INSERT INTO categories (name) VALUES ('clothes'), ('cars'), ('food');
 
 -- insert products into products table
-INSERT INTO products VALUES (1,'Black Dress',55.0,'ASOS Extreme Fringe Back Maxi Dress',1,'./images/black_dress.jpg',1),(1, 'Red Shorts', 38.50,'ASOS PETITE Culotte Shorts',0,'./images/red_shorts.jpg',2),(1,'Blue Top', 28.0,'ASOS Denim Top in Midwash Blue With Exaggerated Shoulder',0,'./images/blue_top.jpg',3),(3,'Warm asian soup', 5.50,'Miso Soup',0,'./images/miso_soup.jpeg',4),(2,'Red, beautiful, fast, expensive', 220000.0,'Ferrari',1,'./images/red_ferrari.jpg',5),(2,'Older model regular black Ford',28950.0,'Ford',0,'./images/ford.jpeg',6),(2,'Black with white stripe, sports', 37650.0,'Mini Cooper',1,'./images/mini_cooper.jpg',7),(3,'Medium cooked meat', 52.0,'Steak',1,'./images/steak.jpg',8),(3,'Warm boiled peas', 4.35,'Peas',1,'./images/peas.jpg',9),(3,'Hot liquid chocolate in center', 15.75,'Chocolate Souffle',1,'./images/chocolate_souffle.jpg',10);
+INSERT INTO products (category, description, price, title, favorite, img_url) VALUES (1,'Black Dress',55.0,'ASOS Extreme Fringe Back Maxi Dress',1,'./images/black_dress.jpg'),(1, 'Red Shorts', 38.50,'ASOS PETITE Culotte Shorts',0,'./images/red_shorts.jpg'),(1,'Blue Top', 28.0,'ASOS Denim Top in Midwash Blue With Exaggerated Shoulder',0,'./images/blue_top.jpg'),(3,'Warm asian soup', 5.50,'Miso Soup',0,'./images/miso_soup.jpeg'),(2,'Red, beautiful, fast, expensive', 220000.0,'Ferrari',1,'./images/red_ferrari.jpg'),(2,'Older model regular black Ford',28950.0,'Ford',0,'./images/ford.jpeg'),(2,'Black with white stripe, sports', 37650.0,'Mini Cooper',1,'./images/mini_cooper.jpg'),(3,'Medium cooked meat', 52.0,'Steak',1,'./images/steak.jpg'),(3,'Warm boiled peas', 4.35,'Peas',1,'./images/peas.jpg'),(3,'Hot liquid chocolate in center', 15.75,'Chocolate Souffle',1,'./images/chocolate_souffle.jpg');
 
-
+SELECT categories.name, products.* FROM products LEFT JOIN categories ON categories.id=products.category group by categories.name order by categories.name, products.title asc;
